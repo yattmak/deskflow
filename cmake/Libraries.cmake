5eb19673d6de46de0919ed9c504e2da532666175
bear0808 <154102353+yattmak@users.noreply.github.com>
2026-06-09 21:29:41 +0900

    fix: bundle Qt SVG support on macOS

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -19,7 +19,7 @@ macro(configure_libs)
     )
   endif()
 
-  find_package(Qt6 ${REQUIRED_QT_VERSION} REQUIRED COMPONENTS Core Widgets Network)
+  find_package(Qt6 ${REQUIRED_QT_VERSION} REQUIRED COMPONENTS Core Widgets Network Svg)
   if(UNIX AND NOT APPLE)
       find_package(Qt6 ${REQUIRED_QT_VERSION} REQUIRED COMPONENTS DBus Xml)
   endif()
